9aa9058012b4dab8281caff554dbbb4329850d06
viniciusferraria <vinicius.ferraria@conductor.com.br>
2019-07-02 16:36:35 -0300

    Added dynamic text to button

--- src/Button/button.js
+++ src/Button/button.js
@@ -2,15 +2,26 @@ import React, { Component } from 'react';
 import './button.css';
 
 export default class Button extends Component{
-    
+    constructor(props) {
+        super(props)
+        this.state = { name: this.props.name }
+        this.handleClick = this.handleClick.bind(this)
+    }
+
     handleClick = () => {
         alert("hello");
+        if (this.state.name === 'Vini') {
+            this.setState({ name: 'Thor' })
+        } else {
+            this.setState({ name: 'Vini' })
+        }
+
     }
-    
-    render(){
-        return(
+
+    render() {
+        return (
             <div>
-                <button onClick={this.handleClick}>Confirm</button>
+                <button onClick={this.handleClick}>{this.state.name}</button>
             </div >
         )
     }
--- src/Sidebar/sidebar.js
+++ src/Sidebar/sidebar.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
 import './sidebar.css';
 
-export default class Sidebar extends Component {
+export default class Sidebar extends Component{
+    constructor(props) {
+        super(props)
+        this.state = { name: 'v' }
+    }
+
     render() {
+        let name = this.props.name;
+        if (name !== this.state.name) {
+            this.setState({ name: name });
+        }
         return (
-            <div >
+            <div>
                 <ul>
-                    <li><a href="../Page1/page1">Page 1</a></li>
-                    <li>{this.props.name}</li>
+                    <li><a href="../Page1/page1" onClick={this.handleClick}>Page 1</a></li>
+                    <li>{this.state.name}</li>
                 </ul>
             </div>
         )
