d70bbba4066d8e36fc298c15b10bd8b840ad7c25
viniciusferraria <vinicius.ferraria@conductor.com.br>
2019-07-08 15:15:01 -0300

    adds textfield

--- src/Main/main.js
+++ src/Main/main.js
@@ -3,6 +3,7 @@ import Header from '../Header/header';
 import Sidebar from '../Sidebar/sidebar';
 import Image from '../Image/image';
 import Button from '../Button/button';
+import TextField from '../TextField/textfield';
 import Thor from './Thor.jpg'
 
 class Main extends Component {
@@ -13,13 +14,16 @@ class Main extends Component {
                     <Header />
                 </div>
                 <div className="Sidebar">
-                    <Sidebar name='' />
+                    <Sidebar name='hello' />
                 </div>
                 <div className="Button">
                     <Button name='Vini' />
                 </div>
                 <div className="bird-image">
                     <Image photo={Thor} />
+                </div>
+                <div className="input">
+                    <TextField />
                 </div>
             </div>
         )
