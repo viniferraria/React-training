226d2f1933d37ddfc64f7357fa8005a89df40ff3
viniciusferraria <vinicius.ferraria@conductor.com.br>
2019-08-17 21:23:42 -0300

    lifting

--- src/Image/image.js
+++ src/Image/image.js
@@ -1,35 +1,19 @@
 import React, { Component } from 'react';
 import './image.css';
-import logo from './logo.svg';
-import aqua from './Aqua.jpg';
-import darth from './Darth.jpg';
+// import logo from './logo.svg';
+// import aqua from './Aqua.jpg';
+// import darth from './Darth.jpg';
 
-export default class Image extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            photo: this.props.photo,
-            index: 0
-        }
-        this.handleClick = this.handleClick.bind(this)
+export default class Image extends Component{
+    constructor(props){
+    super(props)
+        this.handleClick = this.props.handleClick
     }
 
-    handleClick = () => {
-        var arr = [darth, logo, aqua]
-        let i = this.state.index
-        if (i < arr.length) {
-            this.setState({
-                photo: arr[i],
-                index: i + 1
-            });
-        } else {
-            this.setState({ photo: this.props.photo, index: 0 })
-        }
-    }
 
-    render() {
-        return (
-            <img src={this.state.photo} alt={"The mighty thor"} onClick={this.handleClick} />
-        )
+render(){
+    return(
+        <img src={this.props.photo} alt={"The mighty thor"} onClick={this.handleClick} />
+    )
     }
 }
--- src/Main/main.js
+++ src/Main/main.js
@@ -5,27 +5,50 @@ import Image from '../Image/image';
 import Button from '../Button/button';
 import TextField from '../TextField/textfield';
 import Thor from './Thor.jpg'
+import logo from '../Image/logo.svg';
+import aqua from '../Image/Aqua.jpg';
+import darth from '../Image/Darth.jpg';
 
 class Main extends Component {
-    render() {
-        return (
-            <div>
-                <div className="Header">
-                    <Header />
-                </div>
-                <div className="Sidebar">
-                    <Sidebar name='hello' />
-                </div>
-                <div className="Button">
-                    <Button name='Vini' />
-                </div>
-                <div className="bird-image">
-                    <Image photo={Thor} />
-                </div>
-                <div className="input">
-                    <TextField />
-                </div>
-            </div>
+    constructor(props){
+    super(props)
+        this.state = {
+        photo: Thor,
+        index: 0,
+        array: [darth, logo, aqua]
+        }
+    this.handleClick = this.handleClick.bind(this)
+    }
+
+handleClick = () => {
+    var arr = this.state.array
+    let i = this.state.index
+    if(i < arr.length){
+        this.setState({photo: arr[i], index: i+1})
+    } else {
+        this.setState({photo: this.props.photo, index: 0})
+    }
+}
+
+render(){
+    return(
+    <div>
+        <div className="Header">
+            <Header />
+        </div>
+        <div className="Sidebar">
+            <Sidebar name='hello'/>
+        </div>
+        <div className="Button">
+            <Button name='Vini'/>
+        </div>
+        <div className="bird-image">
+            <Image photo={Thor} onClick={this.handleClick} />
+        </div>
+        <div className="input">
+            <TextField/>
+        </div>
+    </div>
         )
     }
 }
